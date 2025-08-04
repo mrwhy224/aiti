7262e4857addfb8933dddfbc78b2111a8ed1df37
mrwhy224 <mail@mroohi.ir>
2025-08-04 03:50:11 +0330

    change the locale to fa

--- config/app.php
+++ config/app.php
@@ -77,12 +77,11 @@ return [
     | set to any locale for which you plan to have translation strings.
     |
     */
+    'locale' => 'fa',
 
-    'locale' => env('APP_LOCALE', 'en'),
+    'fallback_locale' => env('APP_FALLBACK_LOCALE', 'fa'),
 
-    'fallback_locale' => env('APP_FALLBACK_LOCALE', 'en'),
-
-    'faker_locale' => env('APP_FAKER_LOCALE', 'en_US'),
+    'faker_locale' => env('APP_FAKER_LOCALE', 'fa_IR'),
 
     /*
     |--------------------------------------------------------------------------
