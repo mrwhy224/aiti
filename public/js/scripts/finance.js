40b232e3469a43e1a307999a108021b2fa1d0abe
mrwhy224 <mail@mroohi.ir>
2025-08-05 00:52:03 +0330

    add data table to finance

---
+++ public/js/scripts/finance.js
@@ -0,0 +1,95 @@
+$(function() {
+	'use strict';
+
+	var dt_basic_table = $('.datatables-basic');
+	var assetPath = $('body').attr('data-asset-path') || '../';
+	loadAndBlockSection('.statistics-body', '/invoice/statistics', function (json) {
+		$('.invoice_count').text(json.data.invoice_count)
+		$('.company_count').text(json.data.company_count)
+		$('.paid_money').text(json.data.paid_money)
+		$('.unpaid_money').text(json.data.unpaid_money)
+	});
+	// DataTable initialization
+	if (dt_basic_table.length) {
+		var dt_basic = dt_basic_table.DataTable({
+			ajax: function(data, callback, settings) {
+				// This function fetches the data for the table.
+				// You would replace this with your actual API endpoint.
+				DataService.getData('/invoice/list')
+					.then(json => callback(json))
+					.catch(error => callback({
+						data: []
+					}));
+			},
+			columns: [
+				// columns according to your data
+				{ data: 'id' },
+				{ data: 'status' },
+				{ data: 'company.name' },
+				{ data: 'total' },
+				{ data: 'created_at' },
+				{ data: 'due_date' },
+				{ data: '' }
+			],
+			columnDefs: [
+				// {
+				// 	// For the 'name' column
+				// 	targets: 1,
+				// 	render: function(data, type, full, meta) {
+				// 		const name = full['title'];
+				// 		// let output, colorClass = '';
+				// 		// if (full['image']) {
+				// 		// 	output = `<img src="${assetPath}images/avatars/${full['image']}" alt="Avatar" width="32" height="32">`;
+				// 		// } else {
+				// 		// 	const states = ['success', 'danger', 'warning', 'info', 'primary'];
+				// 		// 	const initials = (name.match(/\b\w/g) || []).slice(0, 2).join('').toUpperCase();
+				// 		// 	output = `<span class="avatar-content">${initials}</span>`;
+				// 		// 	colorClass = `bg-light-${states[full['root'] % states.length]}`;
+				// 		// }
+				// 		// return `<div class="d-flex justify-content-left align-items-center"><div class="avatar ${colorClass} me-1">${output}</div><div class="d-flex flex-column"><span class="emp_name text-truncate fw-bold">${name}</span></div></div>`;
+				// 	}
+				// },
+				// {
+				// 	// For the status column
+				// 	targets: 3,
+				// 	render: function(data, type, full, meta) {
+				// 		return full['company_manager'] ? data:'(یافت نشد)';
+				// 	}
+				// },
+				{
+					// Actions
+					targets: -1,
+					title: '',
+					orderable: false,
+					render: function(data, type, full, meta) {
+						return `<div class="d-inline-flex"><a class="pe-1 dropdown-toggle hide-arrow text-primary" data-bs-toggle="dropdown">${feather.icons['more-vertical'].toSvg({ class: 'font-small-4' })}</a><div class="dropdown-menu dropdown-menu-end"><a href="javascript:;" class="dropdown-item attributes-record">${feather.icons['external-link'].toSvg({ class: 'font-small-4 me-50' })}Get attributes</a><a href="javascript:;" class="dropdown-item products-record">${feather.icons['external-link'].toSvg({ class: 'font-small-4 me-50' })}Get products</a><a href="javascript:;" class="dropdown-item inventories-record">${feather.icons['external-link'].toSvg({ class: 'font-small-4 me-50' })}Get inventories</a><a href="javascript:;" class="dropdown-item info-record">${feather.icons['info'].toSvg({ class: 'font-small-4 me-50' })}More info</a><a href="javascript:;" class="dropdown-item delete-record">${feather.icons['trash-2'].toSvg({ class: 'font-small-4 me-50' })}Delete</a></div></div><a href="javascript:;" class="item-edit">${feather.icons['edit'].toSvg({ class: 'font-small-4' })}</a>`;
+					}
+				}
+			],
+			order: [
+				[0, 'desc'] // Default sort by the first column in descending order
+			],
+			// The 'dom' option now excludes the buttons ('B') and the header label.
+			dom: '<"d-flex justify-content-between align-items-center mx-0 row"<"col-sm-12 col-md-6"l><"col-sm-12 col-md-6"f>>t<"d-flex justify-content-between mx-0 row"<"col-sm-12 col-md-6"i><"col-sm-12 col-md-6"p>>',
+			displayLength: 7,
+			lengthMenu: [7, 10, 25, 50, 100],
+			// The buttons array is now empty.
+			buttons: [],
+			language: {
+				search: "جستجو:",
+				lengthMenu: "نمایش _MENU_ رکورد",
+				emptyTable: "هیچ داده‌ای در جدول وجود ندارد",
+				zeroRecords: "هیچ رکوردی با این مشخصات یافت نشد",
+				info: "نمایش _START_ تا _END_ از _TOTAL_ رکورد",
+				infoEmpty: "نمایش 0 تا 0 از 0 رکورد",
+				infoFiltered: "(فیلتر شده از _MAX_ رکورد کل)",
+				loadingRecords: "در حال بارگذاری...",
+				processing: "در حال پردازش...",
+				paginate: {
+					previous: '&nbsp;',
+					next: '&nbsp;'
+				}
+			}
+		});
+	}
+});
--- resources/views/layouts/vuexy/pages/admin/finance.blade.php
+++ resources/views/layouts/vuexy/pages/admin/finance.blade.php
@@ -1,5 +1,23 @@
 @extends('layouts.vuexy.layout')
 @section('title', 'مالی')
+@push('vendor_js')
+    <script src="{{ asset('vendors/js/extensions/jstree.min.js') }}"></script>
+    <script src="{{ asset('vendors/js/tables/datatable/jquery.dataTables.min.js') }}"></script>
+    <script src="{{ asset('vendors/js/tables/datatable/dataTables.bootstrap5.min.js') }}"></script>
+    <script src="{{ asset('vendors/js/tables/datatable/dataTables.responsive.min.js') }}"></script>
+    <script src="{{ asset('vendors/js/tables/datatable/responsive.bootstrap5.min.js') }}"></script>
+    <script src="{{ asset('vendors/js/tables/datatable/vfs_fonts.js') }}"></script>
+    <script src="{{ asset('vendors/js/tables/datatable/buttons.html5.min.js') }}"></script>
+    <script src="{{ asset('vendors/js/tables/datatable/dataTables.rowGroup.min.js') }}"></script>
+@endpush
+@push('page_js')
+    <script src="{{ asset('js/scripts/finance.js') }}"></script>
+@endpush
+@push('styles_before_theme')
+    <link rel="stylesheet" type="text/css" href="{{ asset('vendors/css/tables/datatable/dataTables.bootstrap5.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('vendors/css/tables/datatable/responsive.bootstrap5.min.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('vendors/css/tables/datatable/buttons.bootstrap5.min.css') }}">
+@endpush
 @section('content')
     <div class="content-header row">
         <div class="content-header-left col-md-9 col-12 mb-2">
@@ -15,5 +33,92 @@
                 </div>
             </div>
         </div>
+    </div>
+    <div class="col-12">
+        <div class="card card-statistics">
+            <div class="card-header">
+                <h4 class="card-title">آمار</h4>
+            </div>
+            <div class="card-body statistics-body">
+                <div class="row">
+                    <div class="col-xl-3 col-sm-6 col-12 mb-2 mb-xl-0">
+                        <div class="d-flex flex-row">
+                            <div class="avatar bg-light-primary me-2">
+                                <div class="avatar-content">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trending-up avatar-icon"><polyline points="23 6 13.5 15.5 8.5 10.5 1 18"></polyline><polyline points="17 6 23 6 23 12"></polyline></svg>
+
+                                </div>
+                            </div>
+                            <div class="my-auto">
+                                <h4 class="fw-bolder mb-0 invoice_count">0</h4>
+                                <p class="card-text font-small-3 mb-0">فاکتور ها</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 col-sm-6 col-12 mb-2 mb-xl-0">
+                        <div class="d-flex flex-row">
+                            <div class="avatar bg-light-info me-2">
+                                <div class="avatar-content">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user avatar-icon"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path><circle cx="12" cy="7" r="4"></circle></svg>
+                                </div>
+                            </div>
+                            <div class="my-auto">
+                                <h4 class="fw-bolder mb-0 company_count">0</h4>
+                                <p class="card-text font-small-3 mb-0">اعضا</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 col-sm-6 col-12 mb-2 mb-sm-0">
+                        <div class="d-flex flex-row">
+                            <div class="avatar bg-light-success me-2">
+                                <div class="avatar-content">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-dollar-sign avatar-icon"><line x1="12" y1="1" x2="12" y2="23"></line><path d="M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6"></path></svg>
+                                </div>
+                            </div>
+                            <div class="my-auto">
+                                <h4 class="fw-bolder mb-0 paid_money">0</h4>
+                                <p class="card-text font-small-3 mb-0">پرداختی </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 col-sm-6 col-12">
+                        <div class="d-flex flex-row">
+                            <div class="avatar bg-light-danger me-2">
+                                <div class="avatar-content">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-dollar-sign avatar-icon"><line x1="12" y1="1" x2="12" y2="23"></line><path d="M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6"></path></svg>
+                                </div>
+                            </div>
+                            <div class="my-auto">
+                                <h4 class="fw-bolder mb-0 unpaid_money">0</h4>
+                                <p class="card-text font-small-3 mb-0">بدهی </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="content-body">
+        <section id="basic-datatable">
+            <div class="row">
+                <div class="col-12">
+                    <div class="card">
+                        <table class="datatables-basic table">
+                            <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>وضیعت</th>
+                                <th>عنوان فاکتور</th>
+                                <th>مجموع فاکتور</th>
+                                <th>تاریخ ایجاد</th>
+                                <th>تاریخ پرداخت</th>
+                                <th>عملیات</th>
+                            </tr>
+                            </thead>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </section>
     </div>
 @endsection
