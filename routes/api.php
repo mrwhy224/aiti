85975cdc2c187223ce7a2cdf95fa68698944261f
mrwhy224 <mail@mroohi.ir>
2025-08-05 00:49:58 +0330

    add company and invoice routes

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,20 @@
 <?php
 
-use App\Models\Post;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::group(['prefix'=>'v1'], function () {
+
     Route::group(['prefix'=>'post'], function () {
         Route::get('list', [\App\Http\Controllers\API\PostController::class, 'index']);
+    });
+
+    Route::group(['prefix'=>'company'], function () {
+        Route::get('list', [\App\Http\Controllers\API\CompanyController::class, 'index']);
+        Route::get('pending', [\App\Http\Controllers\API\CompanyController::class, 'pending']);
+    });
+
+    Route::group(['prefix'=>'invoice'], function () {
+        Route::get('list', [\App\Http\Controllers\API\InvoiceController::class, 'index']);
+        Route::get('statistics', [\App\Http\Controllers\API\InvoiceController::class, 'statistics']);
     });
 });
